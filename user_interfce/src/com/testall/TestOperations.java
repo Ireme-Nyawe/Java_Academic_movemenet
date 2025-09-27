1334f33e06a32058eec67bded73ad4801347ea88
Ireme-Nyawe <akimana.inono@gmail.com>
2025-09-27 09:42:03 +0200

    remove ln/mt not for submission

--- user_interfce/src/com/testall/TestOperations.java
+++
@@ -1,9 +0,0 @@
-package com.testall;
-
-import com.entities.Operations;
-
-public class TestOperations {
-    public static void main(String[] args) {
-        Operations op = new Operations();
-    }
-}
